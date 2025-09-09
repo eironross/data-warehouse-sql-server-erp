54150627f1c9a85e1b71392646d9fa4008e5f8da
Flores <eiron.ross.flores@accenture.com>
2025-09-09 23:13:11 +0800

    Updating the mock csv files

--- sql-scripts/init.sql
+++ sql-scripts/init.sql
@@ -1,15 +1,10 @@
 -- medallion architecture + star schema implementation
 
 -- Landing Zone of the Data
-CREATE DATABASE DBWAREHOUSE_STG
+CREATE DATABASE DBWAREHOUSE
 
-USE DBWAREHOUSE_STG;
+USE DBWAREHOUSE;
 GO
 CREATE SCHEMA bronze
-
-CREATE DATABASE DBWAREHOUSE_CURATED
--- Create a pipeline from bronze layer to silver
-USE DBWAREHOUSE_CURATED;
-GO
 CREATE SCHEMA silver
 CREATE SCHEMA gold
